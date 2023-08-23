33b3fe524e9964c9cda0584003178e1cb378ddef
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2023-08-23 20:04:02 +0900

    chore: 웹훅 테스트

--- src/test/kotlin/edu/handong/cseemileage/mileage/semester/service/MileageSemesterServiceTests.kt
+++ src/test/kotlin/edu/handong/cseemileage/mileage/semester/service/MileageSemesterServiceTests.kt
@@ -68,7 +68,7 @@ class MileageSemesterServiceTests @Autowired constructor(
 
     @DisplayName("integration: bulk insert 성능 테스트 - 학기별 마일리지 항목 생성(multiple)")
     @Test
-    @Profile("dev")
+    @Profile("dev") // CI환경에서만 실행
     fun createSemesterMultipleBulkInsert() {
         // Given
         val item1 = itemRepository.findAllByName("전공 항목1").get(0)
