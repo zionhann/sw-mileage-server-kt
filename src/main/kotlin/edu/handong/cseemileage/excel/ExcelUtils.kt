d1384f817ca6b9188ebc6aadf2ae052769d582c0
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2023-09-11 12:25:00 +0900

    Feat: 엑셀 다운로드 스타일 수정 #56 (#60)

--- src/main/kotlin/edu/handong/cseemileage/excel/ExcelUtils.kt
+++ src/main/kotlin/edu/handong/cseemileage/excel/ExcelUtils.kt
@@ -2,8 +2,9 @@ package edu.handong.cseemileage.excel
 
 import edu.handong.cseemileage.excel.dto.ExcelDto
 import edu.handong.cseemileage.excel.strategy.DownloadStrategy
+import edu.handong.cseemileage.excel.strategy.Global
+import edu.handong.cseemileage.excel.strategy.SemesterIn
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
-import org.apache.poi.hssf.util.HSSFColor
 import org.apache.poi.ss.usermodel.BorderStyle
 import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.FillPatternType
@@ -30,30 +31,61 @@ class ExcelUtils(
 
         val headerSize = excelDtoList.size
 
-        // 테이블 헤더 스타일 설정
+        // 기본 스타일 설정
+        val basicStyle = workbook.createCellStyle()
+        // 가운데 정렬
+        basicStyle.alignment = HorizontalAlignment.CENTER
+        // 폰트 크기
+        val font = workbook.createFont()
+        font.fontHeightInPoints = 14.toShort()
+        basicStyle.setFont(font)
+
+        // 헤더 스타일 설정
         val headerStyle = workbook.createCellStyle()
-        // 경계선 설정
+        headerStyle.cloneStyleFrom(basicStyle)
+        // border
         headerStyle.borderTop = BorderStyle.THIN
         headerStyle.borderBottom = BorderStyle.THIN
         headerStyle.borderLeft = BorderStyle.THIN
         headerStyle.borderRight = BorderStyle.THIN
         // 색상
-        headerStyle.fillForegroundColor = HSSFColor.HSSFColorPredefined.GREY_40_PERCENT.index
+        headerStyle.fillForegroundColor = downloadStrategy.getHSSFColor()
         headerStyle.fillPattern = FillPatternType.SOLID_FOREGROUND
-        // 헤더 가운데 정렬
-        headerStyle.alignment = HorizontalAlignment.CENTER
 
         // 헤더 생성
         row = sheet.createRow(rowNo++)
-        for (i in 0 until headerSize) {
-            cell = row.createCell(i)
-            cell.cellStyle = headerStyle
-            cell.setCellValue(excelDtoList[i].columnName)
+        if (downloadStrategy is Global || downloadStrategy is SemesterIn) {
+            for (i in 0 until headerSize) {
+                cell = row.createCell(i)
+                val cellStyle = workbook.createCellStyle()
+                cellStyle.cloneStyleFrom(headerStyle)
+                cell.cellStyle = when (excelDtoList[i].type) {
+                    DownloadStrategy.EXCEL_DTO_CATEGORY -> {
+                        cellStyle.fillForegroundColor = downloadStrategy.getCategoryHSSFColor()
+                        cellStyle
+                    }
+                    DownloadStrategy.EXCEL_DTO_ITEM -> {
+                        cellStyle.fillForegroundColor = downloadStrategy.getItemHSSFColor()
+                        cellStyle
+                    }
+                    DownloadStrategy.EXCEL_DTO_SEMESTER -> {
+                        cellStyle.fillForegroundColor = downloadStrategy.getSemesterHSSFColor()
+                        cellStyle
+                    }
+                    else -> cellStyle
+                }
+                cell.setCellValue(excelDtoList[i].columnName)
+            }
+        } else {
+            for (i in 0 until headerSize) {
+                cell = row.createCell(i)
+                cell.cellStyle = headerStyle
+                cell.setCellValue(excelDtoList[i].columnName)
+            }
         }
 
         // 내용 생성
         val list = downloadStrategy.getList()
-
         if (list != null) {
             for (o in list) {
                 val dataRow = sheet.createRow(rowNo++)
@@ -66,6 +98,7 @@ class ExcelUtils(
                             excelDtoList[columnIndex].type
                         )
                     }
+                    cell.cellStyle = basicStyle
                     cell.setCellValue((value ?: "").toString())
                 }
             }
--- src/main/kotlin/edu/handong/cseemileage/excel/strategy/ItemOnly.kt
+++ src/main/kotlin/edu/handong/cseemileage/excel/strategy/ItemOnly.kt
@@ -9,6 +9,10 @@ class ItemOnly(
 ) : DownloadStrategy {
     override var semester: String = ""
     override var description: String = "글로벌 항목 단독 조회"
+    override fun getHSSFColor(): Short {
+        return super.getItemHSSFColor()
+    }
+
     override fun getExcelDtoList(): List<ExcelDto> {
         val list: MutableList<ExcelDto> = ArrayList()
         addItemColumns(list)
