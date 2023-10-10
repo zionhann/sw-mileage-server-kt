6a3ddba6d76152c213045451a16ff7191d650fdc
한시온(HAN, SION) <hanzion1108@gmail.com>
2023-10-10 11:59:15 +0900

    Feat: 마일리지 항목 엑셀 가져오기 (#99)

--- admin-api/src/main/kotlin/edu/handong/cseemileage/controller/ExcelController.kt
+++ admin-api/src/main/kotlin/edu/handong/cseemileage/controller/ExcelController.kt
@@ -1,5 +1,7 @@
 package edu.handong.cseemileage.controller
 
+import edu.handong.cseemileage.domain.mileage.MileageRecord
+import edu.handong.cseemileage.domain.mileage.SemesterItem
 import edu.handong.cseemileage.excel.ExcelUtils
 import edu.handong.cseemileage.excel.strategy.CategoryOnly
 import edu.handong.cseemileage.excel.strategy.DownloadStrategy
@@ -7,23 +9,30 @@ import edu.handong.cseemileage.excel.strategy.Global
 import edu.handong.cseemileage.excel.strategy.ItemOnly
 import edu.handong.cseemileage.excel.strategy.SemesterIn
 import edu.handong.cseemileage.excel.strategy.SemesterOnly
+import edu.handong.cseemileage.exception.mileage.excel.NotSupportedFileTypeException
 import edu.handong.cseemileage.exception.mileage.semesterItem.SemesterPatternException
 import edu.handong.cseemileage.repository.mileage.CategoryRepository
 import edu.handong.cseemileage.repository.mileage.ItemRepository
 import edu.handong.cseemileage.repository.mileage.SemesterItemRepository
+import edu.handong.cseemileage.service.excel.ExcelService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.Parameters
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.apache.tomcat.util.http.fileupload.IOUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayInputStream
 import javax.servlet.http.HttpServletResponse
 
@@ -33,7 +42,8 @@ import javax.servlet.http.HttpServletResponse
 class ExcelController @Autowired constructor(
     val categoryRepository: CategoryRepository,
     val itemRepository: ItemRepository,
-    val semesterItemRepository: SemesterItemRepository
+    val semesterItemRepository: SemesterItemRepository,
+    val excelService: ExcelService
 ) {
     companion object {
         const val EXCEL_TYPE_CATEGORY_ONLY = "category"
@@ -41,6 +51,7 @@ class ExcelController @Autowired constructor(
         const val EXCEL_TYPE_SEMESTER_ONLY = "semester"
         const val EXCEL_TYPE_GLOBAL = "global"
         const val EXCEL_TYPE_SEMESTER_IN = "semesterIn"
+        const val EXCEL_TYPE_MILEAGE_RECORD = "mileageRecord"
     }
 
     /**
@@ -52,17 +63,26 @@ class ExcelController @Autowired constructor(
      *
      * @param excelType 엑셀 파일의 종류: category, item, semester, global, semesterIn
      * */
-    @Operation(summary = "엑셀 다운로드 by excelType(다운로드 전략)", description = "전략 종류: category, item, semesterItem, global, semesterIn")
+    @Operation(
+        summary = "엑셀 다운로드 by excelType(다운로드 전략)",
+        description = "전략 종류: category, item, semesterItem, global, semesterIn"
+    )
     @Parameters(
         value = [
-            Parameter(name = "excelType", description = "다운로드 전략", required = true, example = "category"),
-            Parameter(name = "semester", description = "학기", required = false, example = "2023-02")
+            Parameter(
+                name = "excelType",
+                description = "다운로드 전략",
+                required = true,
+                example = "category"
+            ), Parameter(name = "semester", description = "학기", required = false, example = "2023-02")
         ]
     )
     @ApiResponses(
         value = [
-            ApiResponse(responseCode = "200", description = "엑셀 다운로드 성공"),
-            ApiResponse(responseCode = "500", description = "엑셀 다운로드 실패 - 학기 형식 오류")
+            ApiResponse(responseCode = "200", description = "엑셀 다운로드 성공"), ApiResponse(
+                responseCode = "500",
+                description = "엑셀 다운로드 실패 - 학기 형식 오류"
+            )
         ]
     )
     @GetMapping("/download/{excelType}")
@@ -93,6 +113,60 @@ class ExcelController @Autowired constructor(
             val excelUtils = ExcelUtils(it)
             val stream: ByteArrayInputStream = excelUtils.createListToExcel()
             IOUtils.copy(stream, response.outputStream)
+        }
+    }
+
+    @Operation(
+        summary = "엑셀 업로드",
+        description = "학기별 항목: semesterIn, 마일리지 기록: mileageRecord"
+    )
+    @Parameters(
+        value = [
+            Parameter(
+                name = "excelType",
+                description = "가져올 마일리지 데이터 종류",
+                required = true,
+                example = "semesterIn"
+            )
+        ]
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "엑셀 업로드 성공",
+        content = [
+            Content(
+                mediaType = "application/json",
+                examples = [
+                    ExampleObject(
+                        value = """
+                            {
+                                "savedIdList": [1, 2, 3, 4, 5]
+                            }
+                        """
+                    )
+                ]
+            )
+        ]
+    )
+    @PostMapping("/upload/{excelType}", consumes = ["multipart/form-data"])
+    fun uploadSemesterItemList(
+        @Parameter(description = "업로드할 엑셀 파일", required = true)
+        @RequestParam("file")
+        file: MultipartFile,
+        @PathVariable excelType: String
+    ): ResponseEntity<Map<String, List<Int>>> {
+        return when (excelType) {
+            EXCEL_TYPE_SEMESTER_IN -> {
+                val savedIdList = excelService.save(file, SemesterItem::class.java)
+                ResponseEntity.ok(mapOf("savedIdList" to savedIdList))
+            }
+
+            EXCEL_TYPE_MILEAGE_RECORD -> {
+                val savedIdList = excelService.save(file, MileageRecord::class.java)
+                ResponseEntity.ok(mapOf("savedIdList" to savedIdList))
+            }
+
+            else -> throw NotSupportedFileTypeException()
         }
     }
 }
--- admin-api/src/main/kotlin/edu/handong/cseemileage/service/excel/ExcelService.kt
+++ admin-api/src/main/kotlin/edu/handong/cseemileage/service/excel/ExcelService.kt
@@ -1,25 +1,68 @@
 package edu.handong.cseemileage.service.excel
 
-import edu.handong.cseemileage.repository.mileage.CategoryRepository
+import edu.handong.cseemileage.domain.mileage.MileageRecord
+import edu.handong.cseemileage.domain.mileage.SemesterItem
+import edu.handong.cseemileage.excel.format.MileageRecordTable
+import edu.handong.cseemileage.excel.format.SemesterItemTable
+import edu.handong.cseemileage.excel.numericToString
+import edu.handong.cseemileage.exception.mileage.excel.NotSupportedFileTypeException
+import edu.handong.cseemileage.repository.mileage.ItemRepository
+import edu.handong.cseemileage.repository.mileage.MileageRecordRepository
+import edu.handong.cseemileage.repository.mileage.SemesterItemRepository
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
 class ExcelService(
-    val categoryRepository: CategoryRepository
+    private val itemRepository: ItemRepository,
+    private val semesterItemRepository: SemesterItemRepository,
+    private val mileageRecordRepository: MileageRecordRepository
 ) {
 
-    fun save(file: MultipartFile) {
+    fun <T> save(file: MultipartFile, classType: Class<T>): List<Int> {
         val workbook = WorkbookFactory.create(file.inputStream)
         val sheet = workbook.getSheetAt(0)
-        val rows = sheet.physicalNumberOfRows
+        val validRows = sheet
+            .filter { row ->
+                val indexCell = row.getCell(0)
 
-        for (i in 1 until rows) {
-            val row = sheet.getRow(i)
-            val cell = row.getCell(0)
-            val value = cell.stringCellValue
-            println(value)
+                indexCell != null &&
+                    numericToString(indexCell) != ""
+            }
+            .toList()
+
+        val results = when (classType.simpleName) {
+            "SemesterItem" -> {
+                val semesterItemList = (1 until validRows.size).map { i ->
+                    val row = sheet.getRow(i)
+                    val excelData = SemesterItemTable(
+                        row,
+                        itemRepository,
+                        semesterItemRepository
+                    )
+                    val item = excelData.toItem()
+                    excelData.toSemesterItem(item)
+                }.toList()
+                semesterItemRepository.saveAll(semesterItemList).map(SemesterItem::id)
+            }
+
+            "MileageRecord" -> {
+                val mileageRecordList = (1 until validRows.size).map { i ->
+                    val row = sheet.getRow(i)
+                    val excelData = MileageRecordTable(
+                        row,
+                        semesterItemRepository,
+                        mileageRecordRepository
+                    )
+                    val semesterItem = excelData.toSemesterItem()
+                    excelData.toMileageRecord(semesterItem)
+                }.toList()
+                mileageRecordRepository.saveAll(mileageRecordList).map(MileageRecord::id)
+            }
+
+            else -> throw NotSupportedFileTypeException()
         }
+        return results
     }
 }
